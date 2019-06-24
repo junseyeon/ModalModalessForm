817932d3bd2ff8a4c7195d44ba6a5d3695881e20
Seyeon Jun <s201718@e-mirim.hs.kr>
2019-06-24 09:11:07 +0900

    #2 Modal & Modaless 창 띄우기 코드 구현 by 3515 전세연

--- ModalModalessForm/Form1.cs
+++ ModalModalessForm/Form1.cs
@@ -16,6 +16,62 @@ namespace ModalModalessForm
             InitializeComponent();
         }
 
-   
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "종료합니다";
+            Application.ExitThread();
+        }
+
+        private void ModalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowModal();
+        }
+
+        private void ShowModal()
+        {
+            this.statusMessage.Text = "모달 창을 띄웁니다";
+            FormModal m = new FormModal();
+            m.Title = "모달 창 띄우기";
+            m.ShowDialog();
+        }
+
+        private void BtmModal_Click(object sender, EventArgs e)
+        {
+            ShowModal();
+        }
+
+        private void BtnModaless_Click(object sender, EventArgs e)
+        {
+            ShowModaless();
+        }
+
+        private void ShowModaless()
+        {
+            this.statusMessage.Text = "모달리스창을 띄웁니다";
+            FormModaless m = new FormModaless();
+            m.Title = "모달리스창";
+            m.Show();
+        }
+
+        private void ModalessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowModaless();
+        }
+
+        private void 이프로그림은ModalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "이 프로그램은.. (Modal)";
+            FormAbout m = new FormAbout();
+            m.Title = "이프로그램은... (Modal)";
+            m.ShowDialog();
+        }
+
+        private void 이프로그램은ModalessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "이 프로그램은.. (Modaless)";
+            FormAbout m = new FormAbout();
+            m.Title = "이프로그램은... (Modaless)";
+            m.Show();
+        }
     }
 }
--- ModalModalessForm/FormAbout.Designer.cs
+++ ModalModalessForm/FormAbout.Designer.cs
@@ -37,9 +37,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(49, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(24, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(651, 484);
+            this.pictureBox1.Size = new System.Drawing.Size(676, 484);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
